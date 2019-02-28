04e0aa562588ee6ca6653c599716b7553a2644ac
katedominjeviar07 <32132248+katedominjeviar07@users.noreply.github.com>
2019-02-28 09:55:46 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,31 @@
+var ans1Nam= "input1";
+ $("button").click(function(){
+     var ans2=$("#ans2").val();
+    var ans1=$("#ans1").val();
+     if ( ans1 >= 6){
+    $("#peri").show();
+}
+
+
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
